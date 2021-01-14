bc7f0dff67e19ec156f6987f7ad179360dd71ad0
TuanLT <tuanlt@onesoft.com.vn>
2021-01-14 10:30:50 +0700

    face detection

--- python-101/python-opencv/p1.py
+++ python-101/python-opencv/p1.py
@@ -1,14 +1,28 @@
 import cv2
+# import sys
 
 camera_id = 0
 
 video = cv2.VideoCapture(camera_id)
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = video.read()
     if ret:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Draw rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
         cv2.imshow("Video", gray)
+
     if cv2.waitKey(1) == ord('q'):
         break
 video.release()
